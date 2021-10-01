feac7085215ca92666094a5faeada3f870692ea1
Sandeep Kandula <sandeep.kandula@in.abb.com>
2021-10-01 12:56:50 +0530

    Building IdentityServer4 Authentication Microservice.

---
+++ SecureMicroservices/Movies.API/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Movies.API.Data;
+
+namespace Movies.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
+        }
+
+        private static void SeedDatabase(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var moviesContext = services.GetRequiredService<MoviesAPIContext>();
+            MovieContextSeed.SeedAsync(moviesContext);
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
